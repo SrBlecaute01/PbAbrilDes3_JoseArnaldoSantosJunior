38e68c2a0f2d222cf49c6f601a3d6d935a4baa12
SrBlecaute01 <josearnaldoma18k@gmail.com>
2024-06-26 16:21:43 -0300

    feat: added base properties for payments microservice.

--- ms-payments/src/test/java/uol/compass/payments/PaymentsApplicationTests.java
+++ ms-payments/src/test/java/uol/compass/payments/PaymentsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MsPaymentsApplicationTests {
+class PaymentsApplicationTests {
 
     @Test
     void contextLoads() {
