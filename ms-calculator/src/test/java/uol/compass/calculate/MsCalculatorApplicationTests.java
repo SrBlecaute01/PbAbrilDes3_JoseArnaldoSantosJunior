750401fd70caef9dc8f422779ab87fe875cc09c7
SrBlecaute01 <josearnaldoma18k@gmail.com>
2024-06-26 15:42:59 -0300

    build: added base project.

---
+++ ms-calculator/src/test/java/uol/compass/calculate/MsCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package uol.compass.calculate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ ms-customer/src/test/java/uol/compass/customer/MsCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package uol.compass.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
