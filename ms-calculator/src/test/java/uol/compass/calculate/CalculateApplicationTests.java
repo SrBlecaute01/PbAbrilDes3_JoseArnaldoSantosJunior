10ec188630c6fe1e15785fd4e498e966c416cb01
SrBlecaute01 <josearnaldoma18k@gmail.com>
2024-06-26 16:23:43 -0300

    feat: added base properties for calculate microservice.

--- ms-calculator/src/test/java/uol/compass/calculate/CalculateApplicationTests.java
+++ ms-calculator/src/test/java/uol/compass/calculate/CalculateApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MsCalculatorApplicationTests {
+class CalculateApplicationTests {
 
     @Test
     void contextLoads() {
