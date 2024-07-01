bb1e312be4d5dc49bbf428b7d5f4abd118529363
SrBlecaute01 <josearnaldoma18k@gmail.com>
2024-07-01 17:38:47 -0300

    feat: added rule service.

--- ms-calculate/src/main/java/uol/compass/calculate/repository/RuleRepository.java
+++ ms-calculate/src/main/java/uol/compass/calculate/repository/RuleRepository.java
@@ -1,6 +1,11 @@
 package uol.compass.calculate.repository;
 
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.web.PagedModel;
 import org.springframework.stereotype.Repository;
 import uol.compass.calculate.model.Rule;
 
@@ -8,5 +13,13 @@ import uol.compass.calculate.model.Rule;
 public interface RuleRepository extends CrudRepository<Rule, Long> {
 
     boolean existsByCategoryIgnoreCase(String category);
+
+    @Query("SELECT COUNT(r.id) > 0 FROM rule r WHERE r.id <> :id AND UPPER(r.category) = UPPER(:category)")
+    boolean existsByCategory(Long id, String category);
+
+    Page<Rule> findAll(Pageable pageable);
+
+    @Transactional
+    Integer deleteRuleById(Long id);
 
 }
