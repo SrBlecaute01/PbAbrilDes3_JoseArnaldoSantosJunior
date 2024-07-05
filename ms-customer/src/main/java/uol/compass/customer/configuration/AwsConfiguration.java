2f5a473814b8d07eb0d1d11e048b7d74a4f2736b
SrBlecaute01 <josearnaldoma18k@gmail.com>
2024-07-05 19:17:58 -0300

    fix: fixed containers communication.

--- ms-customer/src/main/java/uol/compass/customer/configuration/AwsConfiguration.java
+++ ms-customer/src/main/java/uol/compass/customer/configuration/AwsConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
@@ -19,12 +20,18 @@ public class AwsConfiguration {
     @Value("${aws.accessToken}")
     private String accessToken;
 
+    @Value("${aws.region}")
+    private String region;
+
     @Bean
     public S3Client amazonS3() {
         final var credentials = AwsSessionCredentials.create(this.accessKey, this.secretKey, this.accessToken);
         final var provider = StaticCredentialsProvider.create(credentials);
 
-        return S3Client.builder().credentialsProvider(provider).build();
+        return S3Client.builder()
+                .credentialsProvider(provider)
+                .region(Region.of(region))
+                .build();
     }
 
 }
