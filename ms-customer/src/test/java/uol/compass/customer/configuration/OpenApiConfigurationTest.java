ffaacdf66f64d5c5df6e925f67fe64ba486098a9
SrBlecaute01 <josearnaldoma18k@gmail.com>
2024-07-04 22:57:19 -0300

    build: added tests for open api documentation.

---
+++ ms-customer/src/test/java/uol/compass/customer/configuration/OpenApiConfigurationTest.java
@@ -0,0 +1,21 @@
+package uol.compass.customer.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Tests for OpenApi configuration")
+public class OpenApiConfigurationTest {
+
+    private final OpenApiConfiguration configuration = new OpenApiConfiguration();
+
+    @Test
+    @DisplayName("Test for OpenApi configuration")
+    void testOpenApiConfiguration() {
+        final var openApi = this.configuration.openAPI();
+        assertThat(openApi).isInstanceOf(OpenAPI.class);
+    }
+
+}
